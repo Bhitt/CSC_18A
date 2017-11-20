3513f06a26dd098ee3bd92e9603a9426217e3a4d
Astra-Machina <33543588+Astra-Machina@users.noreply.github.com>
2017-11-20 00:05:01 -0800

    Update Skeleslime.java

--- Haunted_House/src/haunted_house/Skeleslime.java
+++ Haunted_House/src/haunted_house/Skeleslime.java
@@ -16,4 +16,20 @@ public class Skeleslime extends Enemy{
     public String printYoSelf(){
         return "";
     }
+    
+    public Integer getHealth()
+    {
+        return this.health;
+    }
+    
+    public Integer getAttack()
+    {
+        return this.attack;   
+    }
+    
+    public Integer skeleslimeAttacks()
+    {
+        System.out.println("The Skeleslime swings a green gelatanous arm at you!");
+        return this.getAttack();
+    }
 }
