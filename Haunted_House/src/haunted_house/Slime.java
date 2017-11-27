f00e80b294550266f98e88b14243306ea3b1de1f
Astra-Machina <33543588+Astra-Machina@users.noreply.github.com>
2017-11-26 16:05:13 -0800

    Update Slime.java

--- Haunted_House/src/haunted_house/Slime.java
+++ Haunted_House/src/haunted_house/Slime.java
@@ -11,10 +11,28 @@ package haunted_house;
  * 
  */
 public class Slime extends Enemy{
-    
+   String type;
+    Integer health;
+    Integer attack;
     
     @Override
     public String printYoSelf(){
         return "";
     }
+    
+    public String getType()
+    {
+        return this.health;
+    }
+    
+    public Integer getHealth()
+    {
+        return this.health;
+    }
+    
+    public Integer getAttack()
+    {
+        return this.attack;   
+    }
+    
 }
