204babf69b41128e5554ea5b28c396570919bc21
Astra-Machina <33543588+Astra-Machina@users.noreply.github.com>
2017-11-26 16:04:01 -0800

    Update Skeleslime.java

--- Haunted_House/src/haunted_house/Skeleslime.java
+++ Haunted_House/src/haunted_house/Skeleslime.java
@@ -11,10 +11,18 @@ package haunted_house;
  * @author ncc
  */
 public class Skeleslime extends Enemy{
+    String type;
+    Integer health;
+    Integer attack;
     
     @Override
     public String printYoSelf(){
         return "";
+    }
+    
+    public String getType()
+    {
+        return this.health;
     }
     
     public Integer getHealth()
@@ -29,6 +37,7 @@ public class Skeleslime extends Enemy{
     
     public Integer skeleslimeAttacks()
     {
+        //IDK if this should be kept or something but for now, this will be here.
         System.out.println("The Skeleslime swings a green gelatanous arm at you!");
         return this.getAttack();
     }
